13190f05b34448ece0fbc5021918c9bd59812972
Maremoto <mmontesto616@gmail.com>
2023-05-31 21:18:34 -0500

    ejercicio 1

---
+++ ejercicio1.py
@@ -0,0 +1,8 @@
+# 1.Write a Python program that takes two numbers as input from the user and prints their sum.
+
+def sum():
+    a = input("Indica primer número para sumar: ")
+    b = input("Indica segundo número para sumar: ")
+    print (f"El resultado es: {float(a) + float(b)}")
+
+sum()
